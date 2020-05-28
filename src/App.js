f24e5bb6c14cffca88dcfa15d140e71ff507cb4b
eric <eric355402@yahoo.com>
2020-05-27 22:30:31 -0500

    SignInWidget is having problems with the @okta css imports, Login has export default withAuth wrapped outside of class which is also causing problems, removing withAuth does not display the signInWidget, end of night

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
+import Home from "./components/pages/Home";
+import Staff from "./components/pages/Staff";
+import Navbar from "./components/layout/Navbar.js";
+import Login from "./components/auth/Login";
+
+function onAuthRequired() {
+  let history = createHistory();
+  history.push("/login");
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Security
+        issuer="https://dev-730560.okta.com/oauth2/default"
+        client_id="0oad8yipeAlEUj2fT4x6"
+        redirect_uri={window.location.origin + "/implicit/callback"}
+        onAuthRequired={onAuthRequired}
+      >
+        <div className="container">
+          <Navbar />
+          <Route path="/" exact={true} component={Home} />
+          <SecureRoute path="/staff" exact={true} component={Staff} />
+          <Route
+            path="/login"
+            render={() => <Login baseUrl="https://dev-730560.okta.com" />}
+          />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+        </div>
+      </Security>
+    </Router>
   );
 }
 
