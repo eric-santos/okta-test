4f59114d6cbc4cff45b7438d2c753db13a8fb181
eric <eric355402@yahoo.com>
2020-05-28 22:29:41 -0500

    defined okta imports by adding okta.d.ts

--- src/components/auth/SignInWidget.js
+++ src/components/auth/SignInWidget.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import OktaSignIn from "@okta/okta-signin-widget";
-// import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
-// import "@okta/okta-signin-widget/dist/css/okta-theme.css";
+import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
 
 class SignInWidget extends Component {
   componentDidMount() {
